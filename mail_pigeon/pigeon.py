9de8743ed16020065b175878f0c31106d0af2dd8
ningwenjie <ningwenjie@getech.cn>
2020-03-26 19:31:24 +0800

    添加节假日和周末不发邮件功能

--- mail_pigeon/pigeon.py
+++ mail_pigeon/pigeon.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
+import time as ptime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -24,21 +25,22 @@ class Pigeon:
     def add_jobs(self):
         self.scheduler.add_job(self.tell_alive, trigger='cron', minute="*/5")
 
-        self.scheduler.add_job(self.check_update, trigger='cron', args=[0], hour=17, minute=30)
-        self.scheduler.add_job(self.check_update, trigger='cron', args=[1], hour=17, minute=45)
-        self.scheduler.add_job(self.check_update, trigger='cron', args=[2], hour=18, minute=00)
-        self.scheduler.add_job(self.check_update, trigger='cron', args=[3], hour=18, minute=15)
-        self.scheduler.add_job(self.check_update, trigger='cron', args=[4], hour=18, minute=30)
+        self.scheduler.add_job(self.check_update, trigger='cron', args=[0], day_of_week='mon-fri', hour=17, minute=30)
+        self.scheduler.add_job(self.check_update, trigger='cron', args=[1], day_of_week='mon-fri', hour=17, minute=45)
+        self.scheduler.add_job(self.check_update, trigger='cron', args=[2], day_of_week='mon-fri', hour=18, minute=00)
+        self.scheduler.add_job(self.check_update, trigger='cron', args=[3], day_of_week='mon-fri', hour=18, minute=15)
+        self.scheduler.add_job(self.check_update, trigger='cron', args=[4], day_of_week='mon-fri', hour=18, minute=30)
 
         self.scheduler.add_job(self.send_daily, trigger='cron', hour=19, minute=00)
         print(f"添加任务成功! -- {datetime.now().strftime('%Y-%m-%d %H:%M')} --")
 
     def check_update(self, time):
         user_dict = self.spider.do_crawl(only_check_update=True)
-        for name, is_update in user_dict:
+        for name, is_update in user_dict.items():
             if not is_update:
                 print(f"{name} 还没有写日报，已提醒{time + 1}次")
                 self._send_alert(time, name)
+                ptime.sleep(1)
 
     @staticmethod
     def tell_alive():
@@ -55,9 +57,10 @@ class Pigeon:
         }
 
         mail_msg = '你今天'
-        for i in range(200):
+        ceil = (time*100) + 1
+        for i in range(ceil):
             mail_msg += word_dict[time]
-        mail_msg += "没有写日报哦~ \n\n若一直没写日报，本条信息将从17:30至19:00每间隔10min，提醒一次~\n\n若至19:00还未写日报，将留空您今天的日报"
+        mail_msg += "没有写日报哦~ \n\n若一直没写日报，本条信息将从17:30至19:00每间隔15min，提醒一次~\n\n若至19:00还未写日报，将留空您今天的日报"
 
         ret = True
         try:
@@ -88,6 +91,15 @@ class Pigeon:
 
     def send_daily(self):
         daily_dict = self.spider.do_crawl()
+
+        all_none = True
+        for daily in daily_dict.values():
+            if daily:
+                all_none = False
+        if all_none:
+            print("今天大家都没有写日报，应该是节假日")
+            return False
+
         mail_msg = self.parse(daily_dict)
 
         ret = True
@@ -95,7 +107,7 @@ class Pigeon:
             email_list = [i for i in getter['user'].values()]
 
             msg = MIMEText(mail_msg, 'html', 'utf-8')
-            msg['From'] = sender['email']
+            msg['From'] = formataddr(("宁文杰", sender['email']))
             msg['To'] = ', '.join(email_list)
             msg['Cc'] = ', '.join(getter['copy'])
             msg['Subject'] = "数据智能组日报{}".format(datetime.now().strftime("%Y/%m/%d"))
@@ -113,4 +125,4 @@ class Pigeon:
 
 if __name__ == "__main__":
     p = Pigeon()
-    p._send_alert(0, "")
+    p.send_daily()
